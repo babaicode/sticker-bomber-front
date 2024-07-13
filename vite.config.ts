fab0e878bffe75006b27d7390ca09e81a7438b8d
Andrei Babai <amaracheuski@mountainlift.cz>
2024-07-13 22:57:17 +0200

    fix config because now can not reach it

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Listen on all IP addresses
+    port: 5173,       // The port number to listen on
+    strictPort: true, // Fail if the port is already in use
+  },
 })
