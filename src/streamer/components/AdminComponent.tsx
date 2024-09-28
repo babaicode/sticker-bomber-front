4a306e5d849e8cda5ff0b118eec14d77eea4b25f
Andrei Babai <amaracheuski@mountainlift.cz>
2024-09-28 14:40:21 +0200

    add admin rules

--- src/streamer/components/AdminComponent.tsx
+++ src/streamer/components/AdminComponent.tsx
@@ -1,11 +1,35 @@
+import { useCallback, useEffect, useState } from "react";
 import { AdminComponentProps } from "../interfaces/StreamerInterface";
+import '../styles/AdminComponent.css';
+import { Environment } from "@/environment";
+import { useAlert } from "@/alert/AlertContext";
+import { AdminRuleInterface } from "../interfaces/AdminInterface";
+import axios from "axios";
 
 export const AdminComponent: React.FC<AdminComponentProps> = ({ adminName, adminId }) => {
+  const API_URL = Environment.StickerBomberBackApiURL;
+  const { showAlert } = useAlert();
+  const [rules, setRules] = useState<AdminRuleInterface[] | null>(null);
+
+  const getRulesByAdminId = useCallback(async (adminId: number) => {
+    try {
+      console.log(adminId, 'adminId');
+      const response = await axios.get(`${API_URL}/admin-ruls/by-adminId/${adminId}`);
+      setRules(response.data);
+    } catch (error) {
+      showAlert('An unexpected error occurred', 'error');
+    }
+  }, [API_URL, showAlert]);
+
+  useEffect(() => {
+    if (adminId) {
+      getRulesByAdminId(adminId);
+    }
+  }, [adminId, getRulesByAdminId]);
+
   return (
-    <div className="admin-link-container">
-      <p>Admin</p>
+    <div className="admin-list-container">
       <p>{adminName}</p>
-      <p>{adminId}</p>
     </div>
   );
 };
---
+++ src/streamer/interfaces/AdminInterface.ts
@@ -0,0 +1,4 @@
+export interface AdminRuleInterface {
+  id: number;
+  rule: string;
+}
