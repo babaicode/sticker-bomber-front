5b599b1f5e49390fa754144be172ca9cefc15d91
Andrei Babai <amaracheuski@mountainlift.cz>
2024-11-17 21:16:58 +0100

    add upload admin avatar

---
+++ src/admin/components/UploadAvatarComponent.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import '../styles/UploadAvatarComponent.css';
+import axios from 'axios';
+import { Environment } from '@/environment';
+import { useAlert } from '@/alert/AlertContext';
+
+const UploadAvatarComponent: React.FC = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const { showAlert } = useAlert();
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const fileType = file.type;
+      if (['image/jpeg', 'image/jpg', 'image/png'].includes(fileType)) {
+        setSelectedFile(file);
+        showAlert('File selected successfully!', 'success');
+      } else {
+        showAlert('Only JPEG, JPG, or PNG files are allowed.', 'error');
+        setSelectedFile(null);
+      }
+    }
+  };
+
+  const handleUpload = async () => {
+    if (!selectedFile) {
+      showAlert('No file selected.', 'error');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+    formData.append('adminId', localStorage.getItem('adminId') || '');
+
+    try {
+      await axios.post(`${Environment.StickerBomberBackApiURL}/admin/upload-avatar`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      showAlert('File uploaded successfully!', 'success');
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      showAlert('Failed to upload file. Please try again.', 'error');
+    }
+  };
+
+  return (
+    <div className="upload-container">
+      <h2>Upload Avatar</h2>
+      <input type="file" accept=".jpeg, .jpg, .png" onChange={handleFileChange} />
+      {selectedFile && <p className="file-name">Selected File: {selectedFile.name}</p>}
+      <button className="upload-button" onClick={handleUpload}>
+        Upload
+      </button>
+    </div>
+  );
+};
+
+export default UploadAvatarComponent;
--- src/admin/page/AdminPage.tsx
+++ src/admin/page/AdminPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../styles/AdminPage.css';
+import UploadAvatarComponent from '../components/UploadAvatarComponent';
 
 const AdminPage: React.FC = () => {
   return (
     <div className='container'>
+      <UploadAvatarComponent />
     </div>
   );
 }
