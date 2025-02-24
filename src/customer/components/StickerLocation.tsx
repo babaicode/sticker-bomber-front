c58427ba20b509ecef392b037b2c7267667ef0aa
andreibabai <andreymarachevskiy@gmail.com>
2025-02-24 19:13:48 +0100

    fix style of sticker picker to work well with mobil and pc

--- src/customer/components/StickerLocation.tsx
+++ src/customer/components/StickerLocation.tsx
@@ -1,22 +1,40 @@
 import { useEffect, useState } from "react";
-import '../styles/StickerLocation.css';
+import "../styles/StickerLocation.css";
 
 const StickerLocation = () => {
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 1440);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 1440);
+  const [shouldShowMap, setShouldShowMap] = useState(false);
 
-    useEffect(() => {
-        const handleResize = () => {
-        setIsMobile(window.innerWidth < 768);
-        };
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1440);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    return (
-        <div className={`sticker-details ${isMobile ? "fullscreen" : "sidebar"}`}>
-        <h2>Sticker Location</h2>
+  return (
+    <div>
+      {isMobile ? (
+        !shouldShowMap ? (
+          <button className="button-1 big" role="button" onClick={() => setShouldShowMap(true)}>
+            Open Map
+          </button>
+        ) : (
+          <div className="map fullscreen">
+            <p>Map for mobile</p>
+            <button className="close-button" onClick={() => setShouldShowMap(false)}>
+              ✖ Close
+            </button>
+          </div>
+        )
+      ) : (
+        <div className="map sidebar">
+          <p>Map for PC</p>
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default StickerLocation;
