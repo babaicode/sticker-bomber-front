af945f06acadd430459a60af70fbb630f2b0d3f5
andreibabai <andreymarachevskiy@gmail.com>
2025-02-24 20:13:07 +0100

    add isMobil to sticker picker component

--- src/customer/page/StickerPicker.tsx
+++ src/customer/page/StickerPicker.tsx
@@ -3,16 +3,19 @@ import { useLocation } from "react-router-dom";
 import { StreamerData } from "../interfaces/CustomerInterface";
 import '../styles/StickerPicker.css';
 import StickerLocation from "../components/StickerLocation";
+import { useState } from "react";
 
 const StickerPicker = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 1440);
     const location = useLocation();
+
     const { streamerData, sticker } = location.state as {
         streamerData: StreamerData;
         sticker: Sticker;
     };
 
     return (
-        <div className="container">
+        <div className={`container ${isMobile ? "mobile" : "desktop"}`}>
             <h2>Send to {streamerData.streamerName}</h2>
             <img className="sticker" src={sticker.url} alt={sticker.stickerName} />
             <div className="sticker-info">
