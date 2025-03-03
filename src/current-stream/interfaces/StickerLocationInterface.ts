a506b1ed0122786066e489a1daf282df6650d78f
andreibabai <andreymarachevskiy@gmail.com>
2025-03-03 21:51:49 +0100

    make much better sticker location

--- src/current-stream/interfaces/StickerLocationInterface.ts
+++ src/current-stream/interfaces/StickerLocationInterface.ts
@@ -1,11 +1,13 @@
 export interface CurrentStreamDataDto {
     streamerId: number;
-    stickers: {
-        stickerId: number;
-        stickerName: string;
-        stickerUrl: string;
-        locationX: number;
-        locationY: number;
-        time: number;
-    } [];
-}
+    stickers: StickerOnCurrentStreamDto[];
+};
+
+export interface StickerOnCurrentStreamDto {
+    stickerId: number;
+    stickerName: string;
+    stickerUrl: string;
+    locationX: number;
+    locationY: number;
+    time: number;
+};
